919d9cdba9b4040c8ebd4c915e2be1a7dd2faaae
Bartłomiej Wilczek <bartlomiejwilczek@o2.pl>
2019-06-29 11:59:46 +0200

    goodbye axios, hello fetch

---
+++ src/components/Input/Input.js
@@ -0,0 +1,8 @@
+import React from "react";
+import styled from "styled-components";
+
+const Input = styled.input`
+  border: none;
+  border-bottom: 1px solid black;
+`;
+export default Input;
--- src/views/Root.js
+++ src/views/Root.js
@@ -1,6 +1,5 @@
 import React from "react";
 import GlobalStyle from "../theme/GlobalStyle";
-import axios from "axios";
 import { debounce } from "lodash";
 
 class Root extends React.Component {
@@ -13,21 +12,11 @@ class Root extends React.Component {
   handleInput = debounce(searchValue => {
     this.setState({ searchValue });
 
-    axios
-      .get(
-        `https://images-api.nasa.gov/search?q=${
-          this.state.searchValue
-        }&media_type=image`
-      )
-      .then(response => {
-        const results = response.data.collection.items;
+    fetch(` https://swapi.co/api/people/?search=${this.state.searchValue}`)
+      .then(response => response.json())
+      .then(data => {
+        const results = data.results;
         this.setState({ results });
-        // this.setState.results = response.data.collection.items;
-
-        console.log(results);
-      })
-      .catch(error => {
-        console.log(error);
       });
   }, 500);
 
@@ -38,15 +27,11 @@ class Root extends React.Component {
         <input onChange={e => this.handleInput(e.target.value)} />
 
         <h2>{this.state.searchValue}</h2>
-        {/* <ul>
-          {this.state.results.map(item => (
-            <li>{item.data[0].description}</li>
-          ))}
-        </ul> */}
+
         <ul>
           {this.state.results.map(item => (
             <li>
-              <img src={item.links[0].href} alt="" />
+              <p>{item.name}</p>
             </li>
           ))}
         </ul>
