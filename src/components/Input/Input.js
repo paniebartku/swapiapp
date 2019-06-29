438a7d816bc5cbb9f6cb28b9069f75c63c0f24b8
Bartłomiej Wilczek <bartlomiejwilczek@o2.pl>
2019-06-29 16:31:14 +0200

    destructoring on state

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -2,13 +2,18 @@ import styled from "styled-components";
 
 const Input = styled.input`
   border: none;
-  border-bottom: 1px solid black;
   font-size: 2rem;
   line-height: 4rem;
   letter-spacing: 1px;
-
+  border-bottom: 2px solid darkgrey;
+  background-color: inherit;
+  display: block;
+  width: 100%;
+  margin: none;
+  transition: all 0.3s ease-in-out;
   &:focus {
-    border-bottom: 1px solid red;
+    box-shadow: 0 0 5px rgba(81, 203, 238, 1);
+    border-bottom: none;
   }
 `;
 export default Input;
--- src/components/Item/Item.js
+++ src/components/Item/Item.js
@@ -11,7 +11,7 @@ class Item extends Component {
   handleInput = debounce(searchValue => {
     this.setState({ searchValue });
 
-    fetch(` https://swapi.co/api/people/?search=${this.state.searchValue}`)
+    fetch(` https://swapi.co/api/people/?search=${searchValue}`)
       .then(response => response.json())
       .then(data => {
         const results = data.results;
@@ -21,24 +21,23 @@ class Item extends Component {
   }, 500);
 
   render() {
+    const { results, searchValue } = this.state;
     return (
       <>
         <Input
           onChange={e => this.handleInput(e.target.value)}
           placeholder="write something"
         />
-        <h2>{this.state.searchValue}</h2>
+        <h2>{searchValue}</h2>
         <ul>
-          {this.state.results.map(
-            ({ name, birth_year, height, eye_color }, i) => (
-              <li key={i}>
-                <p>Name: {name}</p>
-                <p>Birth Day: {birth_year}</p>
-                <p>Height: {height}</p>
-                <p>Eye Color: {eye_color}</p>
-              </li>
-            )
-          )}
+          {results.map(({ name, birth_year, height, eye_color }, i) => (
+            <li key={i}>
+              <p>Name: {name}</p>
+              <p>Birth Day: {birth_year}</p>
+              <p>Height: {height}</p>
+              <p>Eye Color: {eye_color}</p>
+            </li>
+          ))}
         </ul>
       </>
     );
